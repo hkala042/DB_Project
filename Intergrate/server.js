dde121a051e750169dfb6fe3ea2e270132578c3e
Landon Leger <143955894+Lando1121@users.noreply.github.com>
2024-03-30 14:04:54 -0400

    Le code

---
+++ Intergrate/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+app.use(express.urlencoded({ extended: true}))
+
+/*------------------------*/
+ 
+app.use('/public', express.static('public'));
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+const clientRouter = require('.\\routes\\client')
+app.use('/client', clientRouter)
+ 
+app.listen(3000)
